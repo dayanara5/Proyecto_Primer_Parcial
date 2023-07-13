f8ca0496dd101d07dbe0850731b55ad423d162aa
dayanara5 <dnbone@espol.edu.ec>
2023-07-12 21:10:56 -0500

    Creación del proyecto

---
+++ Proyecto_Primer_Parcial/src/main/java/ec/edu/espol/proyecto_primer_parcial/Proyecto_Primer_Parcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.proyecto_primer_parcial;
+
+/**
+ *
+ * @author dayan
+ */
+public class Proyecto_Primer_Parcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
